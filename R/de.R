dc5d9d64305995e373ec6e2575f655756a5658a4
lrocconi <66034255+lrocconi@users.noreply.github.com>
2021-06-03 13:24:59 -0400

    Revert "R package build and functions"
    This reverts commit 9564526cd00a6db8300ca33f9b540dfbce53eb2d.

--- R/de.R
+++
@@ -1,6 +0,0 @@
-de <- function(x) {
-  1 + ((nrow(x@frame)/as.data.frame(lme4::getME(x,"l_i"))[1,1]) - 1) *
-    (as.data.frame(lme4::VarCorr(x))[1,"vcov"] / (sum(as.data.frame(lme4::VarCorr(x))[,"vcov"])))
-}
-
-# only works for the intercept
