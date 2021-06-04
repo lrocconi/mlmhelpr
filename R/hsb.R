ea046959070c33001772005c8b6696b26090545b
acircleda <acornrevolution@yahoo.com>
2021-06-04 09:06:59 -0400

    New changes to ICC and Data

---
+++ R/hsb.R
@@ -0,0 +1,20 @@
+#' HSB: High School and Beyond
+#'
+#' A dataset ... This dataset was imported from an SPSS .sav file using `haven` and therefore has variables attached.
+#'
+#' @format A data frame with 7185 rows and 10 11:
+#' \describe{
+#'   \item{id}{school identification number}
+#'   \item{minority}{ethnicity status: other, minority}
+#'   \item{female}{gender status: female, male}
+#'   \item{ses}{socioeconomic status based on a standardized scale constructed from measures of parental occupation, education, and income}
+#'   \item{mathach}{a measure of math achievement}
+#'   \item{size}{school enrollment size}
+#'   \item{catholic}{school sector: public school or catholic school}
+#'   \item{pracad}{proportion of studentd in the academic track}
+#'   \item{disclim}{scale measuring disciplinary climate}
+#'   \item{himinty}{proportion of minority enrollment}
+#'   \item{meanses}{mean SES for each school}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"diamonds"
--- R/icc.R
+++ R/icc.R
@@ -1,23 +1,40 @@
-# icc
-# intra-class correlation
+#' ICC (Intraclass Correlation)
+#'
+#' @param x A model produced using the lme4::lmer() function. This is an object of class merMod and subclass lmerMod.
+#'
+#' @description
+#'
+#' @return A data frame with random effects and their intraclass correlations.
+#'
+#' @references
+#'
+#' @importFrom lme4 VarCorr
+#' @export
+#'
+#' @examples
+
+
 
 icc <- function(x) {
-  grps <- as.data.frame(lme4::VarCorr(x))[,"grp"]
+
+  #set up dfs for ICC
+  varcorr_df <- as.data.frame(lme4::VarCorr(x))
+  grps <- varcorr_df[,"grp"]
   j <- length(grps)
   grp <- vector()
   icc=NULL
+
+  #calculate ICC
   for (i in 1:j) {
-    grp[i] <- as.data.frame(lme4::VarCorr(x))[i,"vcov"] / (sum(as.data.frame(lme4::VarCorr(x))[,"vcov"]))
+    grp[i] <- varcorr_df[i,"vcov"] / (sum(varcorr_df[,"vcov"]))
     icc <- rbind(icc, grp[i])
   }
-  
+
   iccs <- (data.frame(grps, icc=round(icc,3)))
   # rownames(iccs) <- NULL
   return(iccs)
-  
+
 }
-
-
 
 # Works for 2 and 3 level models (should work with any number of levels)
 
