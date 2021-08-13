15ed1c6092e7bb502ac1ac60c1df8ddb9743c6d4
acircleda <acornrevolution@yahoo.com>
2021-08-13 14:58:57 -0400

    added ability to specify multiple variables

--- R/center.R
+++ R/center.R
@@ -20,31 +20,42 @@
 
 
 center <- function(x, type, group=NULL, variables, value=NULL){
+
+  #grand-mean centering ----
   if(type == "grand"){
     #get data used
     df2 <- x@frame
 
-    #grand-mean centering ----
-    #grand-mean - working only for one variable right now
-    #I didn't feel like writing a for-loop yet
-    df2[[paste0(variables, "_grand")]] <-
-      scale(df[[paste0(variables)]], scale=F)
+    # center variables
+    for(i in colnames(df2)){
+      if(i %in% variables){
+
+        df2[[paste0(i, "_grand")]] <-
+          scale(df2[[paste0(i)]], scale=F)
+      } else {NULL}
+    }
 
     #update formula ----
     #get formula
     formula <- x@call[["formula"]]
     #convert to character vecotr
     formula_as_chr <- Reduce(paste, deparse(formula))
+
     #replace terms
-    new_formula <- sub(variables, paste0(variables, "_grand"),
-                       formula_as_chr)
+    variables_df <- as.data.frame(variables)
+
+    for(i in 1:nrow(variables_df)){
+      var_name <- variables_df[i,1]
+      formula_as_chr <- sub(var_name, paste0(var_name, "_grand"),
+                            formula_as_chr)
+    }
 
     #refit model ----
     # these are important, but have not used them yet
     modeltype <- model0_ml@call[[1]]
     use_REML <- model0_ml@call[["REML"]]
 
-    return(lme4::lmer(new_formula, df2, REML=F))
+    return(lme4::lmer(formula_as_chr, df2, REML=F))
 
 
   } else {
@@ -54,6 +65,7 @@ center <- function(x, type, group=NULL, variables, value=NULL){
 
 }
 
-
+# test
 load("misc/models.Rdata")
 center(model0_ml, type="grand", variables="mathach")
+center(model1_ml, type="grand", variables=c("mathach", "ses"))
