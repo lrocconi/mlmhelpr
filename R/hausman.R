1954d9758f02c04fb676a3b8b1022bd1929c6f62
lrocconi <66034255+lrocconi@users.noreply.github.com>
2021-08-25 10:44:39 -0400

    update with notes

--- R/hausman.R
+++ R/hausman.R
@@ -3,20 +3,40 @@ load("misc/models.Rdata")
 # specifying fixed effect model
 model1_ols <- lm(mathach ~ 1 + ses + as.factor(id), data=hsb)
 
+# Centering SES and computing an average ses for each school (meanses)
+hsb$meanses <- ave(hsb$ses, hsb$id)
+hsb$ses.cwc <- hsb$ses - ave(hsb$ses, hsb$id)
+
+# A simplied fixed-effect model using differencing
+model1b_ols <- lm(mathach ~ 1 + ses.cwc, data=hsb)
+
+# Fox p. 731 paragraph starting "Consider, however, the following, likely
+# surprising, fact:" notes that you can also get the fixed effect estimate by
+# including the group mean as a regressor. He states that "if our object is to
+# estimate the individual-level coefficient beta_2 of X, controlling for all
+# group-level differences, it suffices to control for the within- group means,
+# X_bar_i. The test that beta_3, the coefficient of the compositional variable, is
+# 0 is related to the so-called Hausman test." Below is that test.
+model1c_ols <- lm(mathach ~ 1 + ses + meanses, data=hsb)
+summary(model1c_ols)
+
 # random effects model
 model1_ml
 
 
 #coefficients (b_fe - be_re)
-coef_diff <- coef(model1_ols)[2] - lme4::fixef(model1_ml)[2]
+(coef_diff <- coef(model1b_ols)[2] - lme4::fixef(model1_ml)[2])
 
 #variance (var(b_fe) - var(be_re))
-var_diff = vcov(model1_ols)[2,2] - vcov(model1_ml)[2,2]
+(var_diff = vcov(modelb1_ols)[2,2] - vcov(model1_ml)[2,2])
 
 # Fox formula, pg. 732
-z2 <- coef_diff^2/var_diff
+(z2 <- coef_diff^2/var_diff)
 
 pchisq(z2, df = 1, lower.tail = FALSE)
+
+# I think this is working correctly. I think plm and Stata are using different/modified formulas
+# and probably ones more related to panel/longitudinal data and Fox's is more general (?? I think??)
 
 # another method from https://stat.ethz.ch/pipermail/r-help/2011-January/265671.html - I followed this, but got the same result as above
 
@@ -29,5 +49,9 @@ re <- plm::plm(mathach ~ 1 + ses, index="id", data=hsb, model="random")
 
 plm::phtest(fe, re)
 
+#plm::phtest(model1b_ols, re)
 
+# plm::phtest and Stata hausman give different results (based on defaults)
+# I think the difference has to do with how plm treats unbalanced data
+# https://cran.rstudio.com/web/packages/plm/vignettes/B_plmFunction.html
 
