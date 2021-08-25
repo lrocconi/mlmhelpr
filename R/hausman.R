c9afef004a359f2f6711f1535d824e58bfb79580
Anthony Schmidt <51770624+acircleda@users.noreply.github.com>
2021-08-25 18:42:32 -0400

    Added a simulation

--- R/hausman.R
+++ R/hausman.R
@@ -1,5 +1,57 @@
 load("misc/models.Rdata")
 
+# simulate a two-level model - https://github.com/debruine/faux
+
+# note - open and run the ICC function from icc.R
+
+twolevel_fun = function(sub_sd = 1, item_sd=2, error_sd=3) {
+  set.seed(123)
+  dat <- faux::sim_mixed_cc(
+    sub_n = 100,  # subject sample size
+    item_n = 50,  # item sample size
+    grand_i = 10, # overall mean of the score
+    sub_sd = sub_sd,   # SD of subject random intercepts
+    item_sd = item_sd,  # SD of item random intercepts
+    error_sd = error_sd  # SD of residual error
+  )
+
+  #estimate models
+  fe_model <<- lm(y ~ 1 + as.factor(item_id), data=dat)
+  re_model <<- lme4::lmer(y ~ 1 + (1|item_id), data = dat)
+
+
+  #coefficients (b_fe - be_re)
+  coef_diff <- coef(fe_model)[1] - lme4::fixef(re_model)[1]
+  coef_diff <- as.vector(coef_diff)
+  #variance (var(b_fe) - var(be_re))
+  var_diff = vcov(fe_model)[1,1] - vcov(re_model)[1,1]
+
+  # Fox formula, pg. 732
+  z2 <- coef_diff^2/var_diff
+
+  p <- pchisq(abs(z2), df = 1, lower.tail = FALSE)
+
+  res <- list(
+    p.value      = p,
+    parameter    = z2,
+    method       = "Hausman Test")
+  class(res) <- "htest"
+  return(res)
+  intraclass <- icc(re_model)
+  print(intraclass)
+
+}
+
+# high individual level variation
+twolevel_fun(sub_sd = 100, item_sd=10, error_sd=1)
+icc(re_model)
+
+# low individual level variation
+twolevel_fun(sub_sd = 1, item_sd=10, error_sd=1)
+icc(re_model)
+
+# Do the Hausman test results make sense?
+
 # specifying fixed effect model
 model1_ols <- lm(mathach ~ 1 + ses + as.factor(id), data=hsb)
 
@@ -54,4 +106,26 @@ plm::phtest(fe, re)
 # plm::phtest and Stata hausman give different results (based on defaults)
 # I think the difference has to do with how plm treats unbalanced data
 # https://cran.rstudio.com/web/packages/plm/vignettes/B_plmFunction.html
+
+# following phtest from source code ----
+#from https://rdrr.io/cran/plm/src/R/test_general.R
+
+  coef.wi <- coef(model1_ols)
+  coef.re <- lme4::fixef(model1_reml)
+  vcov.wi <- vcov(model1_ols)
+  vcov.re <- vcov(model1_reml)
+  names.wi <- names(coef.wi)
+  names.re <- names(coef.re)
+  common_coef_names <- names.re[names.re %in% names.wi]
+  coef.h <- common_coef_names[!(common_coef_names %in% "(Intercept)")] # drop intercept if included (relevant when between model inputed)
+  if(length(coef.h) == 0) stop("no common coefficients in models")
+  dbeta <- coef.wi[coef.h] - coef.re[coef.h]
+  df <- length(dbeta)
+  dvcov <- vcov.wi[coef.h, coef.h] - vcov.re[coef.h, coef.h]
+
+  stat <- as.numeric(abs(t(dbeta) %*% solve(dvcov) %*% dbeta))
+  pval <- pchisq(stat, df = df, lower.tail = FALSE)
+
+plm::phtest(fe, re)
+
 
